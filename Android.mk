b52a40c7bdde61f32ec1cec0ab2a14c3006cee4c
Rashed Abdel-Tawab <rashed@linux.com>
2019-02-25 09:31:37 -0800

    Always override the AOSP Calendar app

--- Android.mk
+++ Android.mk
@@ -6,5 +6,6 @@ LOCAL_MODULE := Etar-Calendar
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := prebuilt/Etar-Calendar-release-fdroid-signed.apk
+LOCAL_OVERRIDES_PACKAGES := Calendar
 
 include $(BUILD_PREBUILT)
