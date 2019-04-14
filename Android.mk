b471c71e005dabde0bbf0123c1da9cb2360f3d3a
Rashed Abdel-Tawab <rashed@linux.com>
2019-04-13 17:25:20 -0700

    Revert "Always override the AOSP Calendar app"
    This needs to be present for several other apps to correctly function
    
    This reverts commit b52a40c7bdde61f32ec1cec0ab2a14c3006cee4c.

--- Android.mk
+++ Android.mk
@@ -6,6 +6,5 @@ LOCAL_MODULE := Etar-Calendar
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := prebuilt/Etar-Calendar-release-fdroid-signed.apk
-LOCAL_OVERRIDES_PACKAGES := Calendar
 
 include $(BUILD_PREBUILT)
